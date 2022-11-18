c44557683067bde6a6aede9f91bda66c41076ea6
pabloestornut <estornut@gmail.com>
2022-11-18 09:34:47 +0100

    typescript + decentraland

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,15 @@
-var Rainbow = require('rainbowvis.js');
-var myRainbow = new Rainbow();
+import Rainbow from 'rainbowvis.js';
 
-module.exports = function getHelpImStreamingTimeColor(time){
+const rainbow = new Rainbow();
+
+export function getTimeColorHex(time:Date|number|string){
     var date = time instanceof Date ? time : new Date(time);
     var clockTime = getAbsoluteUTCClockTime(date.getUTCHours(), date.getUTCMinutes(), date.getUTCSeconds(), date.getUTCMilliseconds());
     const MAX = getAbsoluteUTCClockTime(23,59,59,999);
 
-    return myRainbow.colorAt(100*clockTime/MAX);
+    return rainbow.colorAt(100*clockTime/MAX);
 
-    function getAbsoluteUTCClockTime(h,m,s,ms){
+    function getAbsoluteUTCClockTime(h:number,m:number,s:number,ms:number){
         return ms + (s*1000) + (m*1000*60) + (h*1000*60*60);
     }
 }
